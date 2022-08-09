61230336931f2a9978104b0b4618d79d03938dd9
likwidsage <midmalequazi@gmail.com>
2022-08-08 22:34:24 -0400

    readme

--- server/main.go
+++ server/main.go
@@ -26,14 +26,10 @@ func handleApiArticles(w http.ResponseWriter, r *http.Request) {
 	allArticles := getAllArticles(db)
 	for i := 0; i < len(allArticles); i++ {
 		spew.Dump(allArticles[i])
-		fmt.Println("**")
 	}
 
 	b, err := json.Marshal(allArticles)
 	errCheck(err)
-	fmt.Println("Marshalled")
-	fmt.Println(b)
-
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(b)
 }
@@ -42,9 +38,8 @@ func main() {
 	mux := http.NewServeMux()
 	// http.HandleFunc("/api/upload", handleApiUpload)
 	mux.HandleFunc("/api/articles/", handleApiArticles)
-	mux.HandleFunc("/api", handleApiRoot)
 	mux.Handle("/", http.FileServer(http.Dir("../client/build")))
 
+	fmt.Print("Server started")
 	http.ListenAndServe(":5000", mux)
-
 }
